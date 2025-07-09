cde97f5ff303bdec5172fe5c1b6f8c6be505fa50
Anna_F <felleranna31@gmail.com>
2025-07-09 18:32:32 +0300

    lesson 09.07

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,59 +2,66 @@ import React, {useEffect, useState} from 'react';
 
 const Contact = () => {
 
-    const [planets, setPlanetName] = useState('')
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        fetch(`https://sw-info-api.herokuapp.com/v1/planets`)
-            .then(res => res.json())
-            .then(data => { setPlanetName(data) ; setLoading(false) })
-            .catch(err => {
-                console.log(err)
-                setPlanetName('ERROR')
-                setLoading(false);
-            })
-
-    }, [])
+    const [planets, setPlanetName] = useState([])
+    // const [loading, setLoading] = useState(true);
 
 
-    if(loading){
-        return <div>
-            <span className="spinner-border spinner-border-sm"></span>
-            Loading..
-        </div>;
+    async function getPlanets() {
+        const res = await fetch(`https://sw-info-api.herokuapp.com/v1/planets`)
+        const data = await res.json()
+        setPlanetName(data.map(item => item.name ))
     }
-    const handleSubmit = (e) => {
-        e.preventDefault();}
+
+    useEffect( () => {
+            getPlanets().then(() => console.log('Were loaded'))
+         }, [])
+
+
+            // .then(res => res.json())
+            // .then(data => { setPlanetName(data) ; setLoading(false) })
+            // .catch(err => {
+            //     console.log(err)
+            //     setPlanetName('ERROR')
+            //     setLoading(false);
+            // })
+
+
+
+
+    // if(loading){
+    //     return <div>
+    //         <span className="spinner-border spinner-border-sm"></span>
+    //         Loading..
+    //     </div>;
+    // }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();}
 
 
 
     return (
         <div className="containerName">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={e => { e.preventDefault(); }}>
 
                 <label htmlFor="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
 
                 <label htmlFor="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
-                <label htmlFor="country">Country</label>
+                <label htmlFor="country">Planet</label>
                 <select id="country" name="country">
-                    {planets.map((planet) => (
-                        <option key={planet.id} value={planet.name}>
-                            {planet.name}
+                    {planets.map((planetName, index) => (
+                        <option key={index} value={planetName}>
+                            {planetName}
                         </option>
-
-                        ))}
-
-
+                    ))}
                 </select>
 
                 <label htmlFor="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+                <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
-                <input type="submit" value="Submit"/>
-
+                <input type="submit" value="Submit" />
             </form>
         </div>
     );
